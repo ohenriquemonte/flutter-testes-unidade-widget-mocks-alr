0c54b6f87d93a0734b0b011a0f567518824f5c39
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-10-29 10:59:14 -0300

    02.05 - Testando o BankModel

--- test/bank_model_test.dart
+++ test/bank_model_test.dart
@@ -2,8 +2,23 @@ import 'package:estilizacao_componentes/models/bank.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Bank model deposit should be 10', () {
+  group('Bank model deposit tests', () {
+    test('Bank model deposit should turn pojnts into 10', () {
+      final bank = BankModel();
+      bank.deposit(10);
+      expect(bank.points, 10);
+    });
+
+    test('Bank model deposit should turn available into 10', () {
+      final bank = BankModel();
+      bank.deposit(10);
+      expect(bank.available, 10);
+    });
+  });
+
+  test('Bank model transfer should turn pojnts into 10', () {
     final bank = BankModel();
-    bank.deposit(10);
+    bank.transfer(10);
+    expect(bank.points, 10);
   });
 }
