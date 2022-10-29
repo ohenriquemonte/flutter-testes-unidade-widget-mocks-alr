2bd20fce12f1bf48146e140267c0d6b931baa548
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-10-29 11:47:37 -0300

    03.10 - Ações

--- test/home_test.dart
+++ test/home_test.dart
@@ -47,4 +47,17 @@ void main() {
       }
     }), findsNWidgets(5));
   });
+
+  testWidgets('When tap Deposit should upload earned in 10', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: BankInherited(
+      child: const Home(),
+    )));
+
+    await tester.tap(find.text('Deposit'));
+    await tester.tap(find.text('Earned'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('\$10.0'), findsOneWidget);
+  });
 }
